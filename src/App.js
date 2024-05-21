31ef79aece186ede31b4696b22f77844763bf78f
Hidy97 <csaladi.alexandra@gmail.com>
2024-05-21 17:55:10 +0200

    Vizsga vége, mentés

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h5>Szótár</h5>
       </header>
+      <div>
+        <h1>Szavak</h1>
+      </div>
+      <div id="jatekmezo">
+        <div>
+          <select>
+            <option></option>
+            <option></option>
+            <option></option>
+          </select>
+        </div>
+        <span className="oszlop">
+          <div>Angol</div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </span>
+        <span className="oszlop">
+          <div>Magyar</div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </span>
+        <span className="oszlop">
+          <div>Visszajelzés</div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </span>
+      </div>
     </div>
   );
 }
